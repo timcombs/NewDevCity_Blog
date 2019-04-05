45f07d51736bc485e2780419184d9c14f5c0cdd2
Tim Combs <tim00combs@gmail.com>
2019-04-04 21:38:24 -0700

    working through the blog tutorial, added modified index.js to use GraphQL to fetch blog post data and pour it through React component, created two blog posts on 04.03.2019, created blog_template.js which is the template that Gatsby will use to create the pages for each blog post.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `newDevCity blog based on Gatsby Default Starter`,
+    description: `newDevCity blog to showcase portfolio projects, code art, coding tips & tricks.`,
+    author: `@deSolidState`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -25,6 +25,28 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: "posts",
+        path: `${__dirname}/src/posts`
+      }
+    },
+    
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1080,
+            },
+          },
+        ],
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
