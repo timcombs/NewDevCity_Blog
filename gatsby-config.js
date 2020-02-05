94238f5ba23486a343787853b31231ce760695b0
Tim Combs <tim00combs@gmail.com>
2020-02-04 19:33:42 -0800

    after many many days, i wrote a bunch of blog posts and fixed the single quote issue in gatsby-config.js.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,55 +1,56 @@
 module.exports = {
   siteMetadata: {
-    title: `newDevCity blog based on Gatsby Default Starter`,
-    description: `newDevCity blog to showcase portfolio projects, code art, coding tips & tricks.`,
-    author: `@deSolidState`,
+    title: 'newDevCity blog based on Gatsby Default Starter',
+    description:
+      'newDevCity blog to showcase portfolio projects, code art, coding tips & tricks.',
+    author: '@deSolidState',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
 
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "posts",
-        path: `${__dirname}/src/posts`
-      }
+        name: 'posts',
+        path: `${__dirname}/src/posts`,
+      },
     },
-    
+
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           {
-            resolve: `gatsby-remark-images`,
+            resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 1080,
             },
           },
           {
-            resolve: `gatsby-remark-prismjs`,
+            resolve: 'gatsby-remark-prismjs',
             options: {
-              classPrefix: "language-",
+              classPrefix: 'language-',
               inlineCodeMarker: null,
               aliases: {},
               showLineNumbers: false,
