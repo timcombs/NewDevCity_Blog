d6bd39ae7c19632b081c7782e2c64876547962f0
Tim Combs <tim00combs@gmail.com>
2020-05-24 18:04:09 -0700

    changed icon to display in the browser. was also working on adding tags. trying to just add and push the icon changes.

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,7 +9,7 @@ const path = require('path');
 
 exports.createPages = ({ actions, graphql }) => {
   const { createPage } = actions;
-  const blogPostTemplate = path.resolve('src/templates/blog_template.js');
+  const blogPostTemplate = path.resolve('src/templates/blog-template.js');
 
   return graphql(`
     {
